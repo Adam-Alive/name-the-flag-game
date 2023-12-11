eede00bfe7b7b038cfd7ffecd5c84c96ee007f79
Adam-Alive <>
2023-12-11 17:36:02 +0000

    Add link to html for js file with questions data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,29 @@
 // Code starts here
 // Let the DOM finish loading before running the game
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-    console.log(buttons);
+  let buttons = document.getElementsByTagName("button");
+  console.log(buttons);
 });
 
 // Randomly selects a flag-country pair - does so as a function, but not when variable
 function displayCurrentFlag() {
-    console.log(flagData[Math.floor(Math.random() * flagData.length)]);
+  console.log(flagData[Math.floor(Math.random() * flagData.length)]);
 }
 displayCurrentFlag();
 
-function displaySecondCountry() { }
+function displaySecondCountry() {}
 
-function displayThirdCountry() { }
+function displayThirdCountry() {}
 
-function shuffleQuestions() { }
+function shuffleQuestions() {}
 
-function correctAnswer() { }
+function correctAnswer() {}
 
-function incorrectAnswer() { }
+function incorrectAnswer() {}
 
-function nextQuestion() { }
+function nextQuestion() {}
 
-function totalScoreMessage() { }
+function totalScoreMessage() {}
 
-function restartGame() { }
+function restartGame() {}
