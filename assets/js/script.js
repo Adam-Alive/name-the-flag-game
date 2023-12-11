880347e1cb5a362e250dd118edfb0554b5456016
Adam-Alive <>
2023-12-11 17:32:16 +0000

    Create new js file for questions data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,17 @@
-// Flag and Country Dictionary
-const flagData = [
-    {
-        flag: "afghanistan.webp",
-        country: "Afghanistan",
-    },
-    {
-        flag: "aland-islands.webp",
-        country: "Aland Islands",
-    },
-
-    {
-        flag: "albania.webp",
-        country: "Albania",
-    },
-    {
-        flag: "american-samoa.webp",
-        country: "American Samoa",
-    },
-    {
-        flag: "andorra.webp",
-        country: "Andorra",
-    },
-    {
-        flag: "angola.webp",
-        country: "Angola",
-    },
-    {
-        flag: "anguilla.webp",
-        country: "Anguilla",
-    },
-    {
-        flag: "antarctica.webp",
-        country: "Antarctica",
-    },
-    {
-        flag: "antigua-and-barbuda.webp",
-        country: "Antigua and Barbuda",
-    },
-    {
-        flag: "argentina.webp",
-        country: "Argentina",
-    },
-];
-// Last line of dictionary directly above this line
-
 // Code starts here
 // Let the DOM finish loading before running the game
-// On page load, the first flag and three countries will be displayed
-// User clicks on a country name to answer
-// Not sure about listening for button clicks??
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
     console.log(buttons);
 });
 
-for (let country of flagData) {
-    console.log(country.country);
-}
-
-for (let flag of flagData) {
-    console.log(flag.flag);
-}
-
 // Randomly selects a flag-country pair - does so as a function, but not when variable
 function displayCurrentFlag() {
     console.log(flagData[Math.floor(Math.random() * flagData.length)]);
 }
 displayCurrentFlag();
-
-let testCountryDiv = document.getElementById("current-flag-country").innerHTML;
-console.log(testCountryDiv);
-
-// Test random select function
-function testCountrySelect() {
-    let currentCountry = country(Math.floor(Math.random() * country.length));
-    console.log(currentCountry);
-}
-
 
 function displaySecondCountry() { }
 
