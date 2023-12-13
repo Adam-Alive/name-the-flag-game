53f40577feb516f1dd3ae56bdb8ba1f4cd1b987d
Adam-Alive <>
2023-12-13 21:46:53 +0000

    Add code to write shuffled questions to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ const optionsContainerDiv = document.getElementById('flag-options-container');
 const restartButton = document.getElementById('restart-btn');
 
 
-let shuffledFlagData, currentFlagIndex
+let shuffledFlagData, currentFlagIndex;
 
 let img = document.getElementById('flag-image');
 
 startButton.addEventListener('click', startGame);
 
 nextButton.addEventListener('click', () => {
-    currentFlagIndex++
-    setNextQuestion()    
+    currentFlagIndex++;
+    // setNextQuestion();
 });
 
 restartButton.addEventListener('click', startGame);
@@ -29,25 +29,25 @@ function startGame() {
     nextButton.classList.remove('hide');
     optionsContainerDiv.classList.remove('hide');
     restartButton.classList.remove('hide');
-// Test code
+    // Test code
     console.log('game has started!');
-// Continue
-    shuffledFlagData = flagData.sort(() => Math.random() -0.5);
+    // Continue
+    shuffledFlagData = flagData.sort(() => Math.random() - 0.5);
     currentFlagIndex = 0;
     showFlagQuestion();
-
+    console.log(shuffledFlagData);
 
 }
 
 // Function to display flag image and 3 answer options
 function showFlagQuestion() {
-    console.log('this is showFlagQuestion!')
+    console.log('this is showFlagQuestion!');
 }
 
 
-function setNextQuestion() { 
-    console.log('this is setNextQuestion!!!')
-}
+// function setNextQuestion() {
+//     console.log('this is setNextQuestion!!!');
+// }
 
 
 
@@ -56,9 +56,6 @@ function test() {
     console.log('next button has been clicked');
 }
 
-// function test2() {
-//     console.log('restart button has been clicked');
-// }
 
 // *** Temporaary code for testing ***
 console.log(flagData);
@@ -68,7 +65,7 @@ console.log(flagData[1]);
 console.log(flagData[2].flag);
 
 for (let flag of flagData) {
-    console.log(flag)
+    console.log(flag);
 }
 
 console.log(flagData[2].options[1]);
@@ -82,14 +79,13 @@ console.log(flagData[3].options[2].correct);
 
 // Possible functions ***
 
+function selectAnswer() { }
+
+function setNextQuestion() { }
 
 function totalScoreMessage() { }
 
-function shuffleQuestions() { }
 
-function incrementScoreTally () { }
-
-function displayAnswer () { }
 
 
 // Code below is for reference
